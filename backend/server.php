83a3f7e3fc1c4910954f857623d44d2e830d68e1
Krystian Barczak <48355396+kryst1anb@users.noreply.github.com>
2020-06-13 11:33:08 +0200

    Dodano komunikat o pierwszym logowaniu

--- backend/server.php
+++ backend/server.php
@@ -1,47 +1,52 @@
 <?php
-    // set_time_limit(0);
-    // while (true){
-    // $key = "rziZsXX2Skmc31Iq6gob3dfgQDMUb4ceowC0";
-    //     $json = file_get_contents("https://currencyapi.net/api/v1/rates?key=$key");
-    //     $obj = json_decode($json);
-    //     $getAPITime = $obj->{'updated'};
-    //     file_put_contents("json/$key-$getAPITime.json", $json);
-    //     file_put_contents($file, $json);
-    //     sleep(3600);
-    // }
-
     $rawdata = file_get_contents("php://input");
     $dataJSON = json_decode($rawdata,true);
     $key = $dataJSON['key'];
     set_time_limit(0);
     if($dataJSON['send']){
-//        while (true){
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, "https://currencyapi.net/api/v1/rates?key=$key");
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            $json = curl_exec($ch);
+    while (true){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://currencyapi.net/api/v1/rates?key=$key");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $json = curl_exec($ch);
+        
+        $obj = json_decode($json,true);
 
-            $obj = json_decode($json,true);
+        if(curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 200) {
+            $date = new DateTime(date("Y-m-d H:i:s"));
+            $getAPITime = date_format($date, 'U');
+            
+            file_put_contents("json/$key-$getAPITime.json", $json);
+            $files = array_combine(glob("json/$key*"), array_map('filectime', glob("json/$key*")));
+            arsort($files);
 
-            if(curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 200) {
-                echo " Error-".$obj['error']['code']."-".$obj['error']['message'];
-            } else{
-                $getAPITime = $obj['updated'];
-                file_put_contents("json/$key-$getAPITime.json", $json);
-
-                $files = array_combine(glob("json/$key*"), array_map('filectime', glob("json/$key*")));
-                arsort($files);
-                $data = json_decode(file_get_contents(key($files)),true);
-
-                echo json_encode($data);
+            $jsonDataToJS = '{"json":[';
+            foreach($files as $k => $v){
+                $jsonDataToJS .= file_get_contents($k, true).',';
             }
+            $jsonDataToJS = substr($jsonDataToJS, 0, -1);
+            $jsonDataToJS .= ']}';
+            print_r(json_decode($jsonDataToJS, true));
+        } else{
+            $getAPITime = $obj['updated'];
+            file_put_contents("json/$key-$getAPITime.json", $json);
 
-            curl_close($ch);
-//            sleep(3600);
-//      }
-    } else
-      {
-          header("Location: http://www.google.com");
-          exit();
+            $files = array_combine(glob("json/$key*"), array_map('filectime', glob("json/$key*")));
+            arsort($files);
+
+            $jsonDataToJS = '{"json":[';
+            foreach($files as $k => $v){
+                $jsonDataToJS .= file_get_contents($k, true).',';
+            }
+            $jsonDataToJS = substr($jsonDataToJS, 0, -1);
+            $jsonDataToJS .= ']}';
+            print_r(json_decode($jsonDataToJS, true));
+        }
+        curl_close($ch);
+        sleep(3600);
       }
+    } else {
+        header("Location: http://www.google.com");
+         exit();
+    }
 ?>
